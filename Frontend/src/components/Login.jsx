45256428938e1601270c3321aa480f45e8e85329
unknown <imranaha310@gmail.com>
2025-01-13 00:27:15 +0530

    integrating front+backend for deploy

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://e-commerrce-3.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
