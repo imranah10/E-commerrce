39fc7dd3e3e642124298ca701db22efd4555c125
MVMHAND <prateek@myvirtulmate.com>
2025-09-06 23:40:01 +0530

    second commit

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    "http://localhost:3000",
     'https://e-commerrce-6ocm.vercel.app'
   ],
   credentials: true
